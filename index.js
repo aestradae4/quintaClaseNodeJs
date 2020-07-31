a5162c2ad240db62990043e59c9a41c27bc3cd50
Emanuel <aestradae4@miumg.edu.gt>
2020-07-31 17:09:11 -0600

    Primer NPM

---
+++ index.js
@@ -0,0 +1,9 @@
+const colors = require('colors');
+
+
+//Core Node (fs, http, events)
+//Utilizar archivos modulos
+// Paquetes instalados con NPM
+
+console.log('Hola mundo'.green);
+console.log('i like cake and pies'.underline.green);
